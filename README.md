7869b521c9de70b53fa8c303aedf723dbdba10c4
Hannah Won <nowhananhwon@gmail.com>
2014-08-21 01:31:51 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 sortingalgorithm
 ================
+
+
+Merge Sort Algorithm referenced from:
+http://en.wikipedia.org/wiki/Merge_sort
---
+++ sort.js
@@ -0,0 +1,33 @@
+var message = "Header";
+var messageSplit = message.split(' '); //creates an array of strings from message
+
+$('#container').html('<p>' + message + '<p>'); //inserts a message
+
+// $.each(index, $(".sortme")) function() {
+//   $this.attr('id=' + index);
+// });
+
+
+// function mergeSort(left, right) {
+//   var sortList = [];
+//   // assign the element of the sublists to 'sortList' variable until there is no element to merge.
+//   while (left.length > 0 || right.length > 0) {
+//     if ((left.length > 0) && (right.length > 0)) {
+//      // compare the first two element, which is the small one, of each two sublists.
+//       var result = []
+//       if (left[0] <= right[0]) {
+//         sortList.append(left[0]);
+//         result.push(left.shift());
+//       } else {
+//         sortList.append(right[0]);
+//         result.push(right.shift());
+//         }
+//     } else if (left.length > 0) {
+//         sortList.append(left[0]);
+//         result.push(left.shift());
+//     } else if (right.length > 0) {
+//         sortList.append(right[0]);
+//         result.push(right.shift());
+//       }
+//   return sortList
+// }}
