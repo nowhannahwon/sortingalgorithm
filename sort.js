da752122ff768881ce21ccd279bc5c24a55a8b40
Hannah Won <nowhananhwon@gmail.com>
2014-08-23 14:52:57 -0700

    Randomizes div values and width on page load

--- sort.js
+++ sort.js
@@ -1,41 +1,44 @@
-$('#header').fadeIn(2000);
+// $('p')
+//   .each(function(index) {
+//   $(this)
+//   .fadeIn(2000)
+//   .css('display', 'block')
+//   .text(index)
+//   .width(index + "%");
+//   });
 
-
-$('p')
-  .each(function(index) {
-  $(this)
-  .fadeIn(2000)
-  .css('display', 'block')
-  .text(index);
+$('p').each(function(index) {
+    rand = Math.floor(Math.random()*20);
+    $(this)
+    .fadeIn(2000)
+    .css('display', 'block')
+    .text(rand)
+    .width(rand + "%");
   });
 
+// $('#buttondiv').click(function() {
+//   alert($('.container').contents().find('p'));
 
-
-// $.each(index, $(".sortme")) function() {
-//   $this.attr('id=' + index);
+    // .each($('p'), (function() {
+    //   $(this).fadeIn(1500, (function() {
+    //     var $next = $(this).next();
+    //     var firstnumber = parseInt($(this).text());
+    //     var secondnumber = parseInt($next.text());
+    //     if (firstnumber < secondnumber) {
+    //       return firstnumber;
+    //     } else {
+    //       $(this).fadeIn(1500, (function() {
+    //       $(this).css('background-color', 'red');
+    //       }));
+    //     }
+    //   }))
+    // }))
 // });
 
-
-// function mergeSort(left, right) {
-//   var sortList = [];
-//   // assign the element of the sublists to 'sortList' variable until there is no element to merge.
-//   while (left.length > 0 || right.length > 0) {
-//     if ((left.length > 0) && (right.length > 0)) {
-//      // compare the first two element, which is the small one, of each two sublists.
-//       var result = []
-//       if (left[0] <= right[0]) {
-//         sortList.append(left[0]);
-//         result.push(left.shift());
-//       } else {
-//         sortList.append(right[0]);
-//         result.push(right.shift());
-//         }
-//     } else if (left.length > 0) {
-//         sortList.append(left[0]);
-//         result.push(left.shift());
-//     } else if (right.length > 0) {
-//         sortList.append(right[0]);
-//         result.push(right.shift());
-//       }
-//   return sortList
-// }}
+    // var secondnumber = $((this) '+ p').val();
+    // if (firstnumber < secondnumber) {
+    //   return firstnumber;
+    // } else {
+    //   secondnumber = firstnumber;
+    //   firstnumber = secondnumber;
+    // }
